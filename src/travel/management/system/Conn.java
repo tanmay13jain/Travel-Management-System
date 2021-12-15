f8ab05941e6b537f19f9034d4057539307bddb88
Tanmay Jain <78400970+tanmay13jain@users.noreply.github.com>
2021-12-15 21:56:14 +0530

    Add files via upload

---
+++ src/travel/management/system/Conn.java
@@ -0,0 +1,20 @@
+package travel.management.system;
+
+import java.sql.*;
+
+public class Conn {
+    Connection c;
+    Statement s;
+    public Conn(){
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/tms","root","tanmay");
+            s = c.createStatement();
+            
+            
+        }catch(Exception e){
+            
+        }
+    }
+}
